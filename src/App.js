df418c308b96844c1e40b8e89c425ba51e0adc7c
Derek Diaz <gigabit77@gmail.com>
2019-07-14 19:56:32 -0500

    Login, Registration and Intro completed

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Components
+import Header from "./components/header"
+import Home from "./routes/home"
+import Login from "./routes/login";
+import Register from "./routes/register";
+import Intro from "./routes/intro";
+
+function App() {
+  return (
+      <Router>
+          <div>
+              <Header />
+              <Route exact path="/" component={Intro} />
+              <Route path="/home" component={Home} />
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register} />
+          </div>
+      </Router>
+  );
+}
+
+export default App;
